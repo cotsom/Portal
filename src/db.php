8ecef944d38c07f14164ef98b6d688e499d3b2fb
Ivan Hahanov <ivanh@naliway.local>
2022-01-31 21:37:23 +0300

    add env variables

--- src/db.php
+++ src/db.php
@@ -1,6 +1,6 @@
 <?php
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	$mysql = new mysqli('mariadb', 'root', 'password', 'users');
+	$mysql = new mysqli($_ENV["DB_URL"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], 'users');
 
 	if (mysqli_connect_errno()) {
 		echo mysqli_connect_errno();
