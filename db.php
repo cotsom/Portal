70cd27be563251f06a3597275c12197436962f97
cotsom <gerfy.17@mail.ru>
2022-01-31 21:09:35 +0300

    test

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	$mysql = new mysqli('mariadb', 'root', 'password', 'users');
+	$mysql = new mysqli('localhost', 'root', 'cotsom17', 'users');
 
 	if (mysqli_connect_errno()) {
 		echo mysqli_connect_errno();
